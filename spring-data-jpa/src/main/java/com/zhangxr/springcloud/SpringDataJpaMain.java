6bd16d836edda3aa92467f1b0e2ae6fb3811c1c2
sdzhangxr <sdzhangxr@126.com>
2020-12-21 17:55:38 +0800

    添加主启动类

---
+++ spring-data-jpa/src/main/java/com/zhangxr/springcloud/SpringDataJpaMain.java
@@ -0,0 +1,18 @@
+package com.zhangxr.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @className SpringDataJpaMain
+ * @Description TODO
+ * @Author sdzha
+ * @Date 2020/12/19 20:27
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class SpringDataJpaMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaMain.class,args);
+    }
+}
