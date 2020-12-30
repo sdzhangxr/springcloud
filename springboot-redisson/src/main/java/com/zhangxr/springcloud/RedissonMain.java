99b7ad26a24833de96ac5c28c1cc0e0fdcf3ed49
sdzhangxr <sdzhangxr@126.com>
2020-12-30 15:42:40 +0800

    Redisson

---
+++ springboot-redisson/src/main/java/com/zhangxr/springcloud/RedissonMain.java
@@ -0,0 +1,16 @@
+package com.zhangxr.springcloud;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @className RedissonMain
+ * @Description Redisson主启动类
+ * @Author sdzha
+ * @Date 2020/12/30 9:41
+ * @Version 1.0
+ */
+public class RedissonMain {
+    public static void main(String[] args) {
+        SpringApplication.run(RedissonMain.class,args);
+    }
+}
